5591fa125ad81a38e9a75bebfc8bfed0e37c5993
znkalena <elze66@gmail.com>
2022-11-24 23:05:50 +0300

    create modul js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import './main.js';
+import './create-new-photo.js';
+import './forma.js';
+import './map.js';
+import './popup.js';
+import './message-succes.js';
+import './message-error.js';
---
+++ js/util.js
@@ -0,0 +1,10 @@
+const returnNumber = (min , max) => {
+  if(min<0 || max<0){
+    return -1;
+  }
+  if (max<min){
+    [min,max]=[max,min];
+  }
+    return Math.floor(min+Math.random()*(max-min+1));
+};
+export{returnNumber};
